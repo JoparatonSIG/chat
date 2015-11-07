6a82a579d91890f62f7a7ddba22c69fff6959ac5
luchobenitez <luchobenitez@gmail.com>
2015-11-07 19:23:50 -0300

    en guarani

---
+++ hackpire/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var ombaapova = express();
+
+ombaapova.get('/_|_', function(jerurepyre, ñeembojevy){
+	ñeembojevy.send('reheshapa');
+}); 
+
+ombaapova.listen(3000);
+
+
